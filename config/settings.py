280489af53ffbf37fa5151a4a18fb87ae779f09c
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-10-05 23:00:36 +0400

    Создана логика верификации пользователя.

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Настройки почтового сервиса
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
