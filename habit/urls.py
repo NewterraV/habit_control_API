2885c748446c4907e6cbf88aeef6b6ab4502b424
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-10-06 12:48:51 +0400

    Создан базовый вывод опубликованых привычек.

--- habit/urls.py
+++ habit/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from habit.apps import HabitConfig
-from habit.views import HabitViewSet
+from habit.views import HabitViewSet, HabitListAPIView, HabitDetailView
 
 app_name = HabitConfig.name
 router = DefaultRouter()
@@ -9,5 +9,6 @@ router.register('habits', HabitViewSet, basename='habits')
 
 
 urlpatterns = [
-
+    path('habits/publish/', HabitListAPIView.as_view(), name='habits_publish'),
+    path('habits/publish/detail/<int:pk>', HabitDetailView.as_view(), name='habits_detail_publish')
 ] + router.urls
--- habit/views.py
+++ habit/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import generics
 
 from habit.models import Habit
 from habit.serializers import HabitSerializer
@@ -9,4 +10,16 @@ class HabitViewSet(viewsets.ModelViewSet):
 
     queryset = Habit.objects.all()
     default_serializers = HabitSerializer
+    serializer_class = HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+
+    queryset = Habit.objects.filter(is_publish=True)
+    serializer_class = HabitSerializer
+
+
+class HabitDetailView(generics.RetrieveAPIView):
+
+    queryset = Habit.objects.filter(is_publish=True)
     serializer_class = HabitSerializer
